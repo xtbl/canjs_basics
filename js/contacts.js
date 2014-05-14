d9620e5c318ede8f0ed95c3cb81cce3493684a60
Cristobal Avila <cavila@spredfast.com>
2014-05-14 14:48:22 -0600

    passing data to template

---
+++ js/contacts.js
@@ -0,0 +1,44 @@
+var CONTACTS = [
+  {
+    id: 1,
+    name: 'William',
+    address: '1 CanJS Way',
+    email: 'william@husker.com',
+    phone: '0123456789',
+    category: 'co-workers'
+  },
+  {
+    id: 2,
+    name: 'Laura',
+    address: '1 CanJS Way',
+    email: 'laura@starbuck.com',
+    phone: '0123456789',
+    category: 'friends'
+  },
+  {
+    id: 3,
+    name: 'Lee',
+    address: '1 CanJS Way',
+    email: 'lee@apollo.com',
+    phone: '0123456789',
+    category: 'family'
+  }
+];
+ 
+var CATEGORIES = [
+  {
+    id: 1,
+    name: 'Family',
+    data: 'family'
+  },
+  {
+    id: 2,
+    name: 'Friends',
+    data: 'friends'
+  },
+  {
+    id: 3,
+    name: 'Co-workers',
+    data: 'co-workers'
+  }
+];
---
+++ js/templates.js
@@ -0,0 +1,5 @@
+<script type="text/mustache" id="todosList">
+{{#todos}}
+<li>{{description}}</li>
+{{/todos}}
+</script>
