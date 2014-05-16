467c7af39c5c56d36a3c1a0daa24c9b1465486cc
Cristobal Avila <cavila@spredfast.com>
2014-05-16 15:37:18 -0600

    component created

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-	var data = {message: "Im  a message from the data object using a mustache template!"};
+	var data = {
+		message: "Im  a message from the data object using a mustache template!",
+		createdAt: new Date()
+	};
 	// Pass the id of the template and the data, containing our message to can.view
 	var frag = can.view("app-template", data);
 	// load the fragment in the page
@@ -24,7 +27,7 @@
 		data : {people: people},
 		state: peopleState
 		});
-	$("#my-app").html(peopleFrag);
+	// $("#people-container").html(peopleFrag);
 
 	$("#add-person-btn").click(function () {
 		people.push({firstname: "New", lastname: "Guy"});
@@ -38,11 +41,58 @@
 		peopleState.attr("visible", !peopleState.attr("visible") );
 	});
 
+	// Handle User Interaction
+	// can.Control
+	var famousPeople = [
+	    {firstname: "John", lastname: "Doe"},
+	    {firstname: "Emily", lastname: "Dickinson"},
+	    {firstname: "William", lastname: "Adams"},
+	    {firstname: "Stevie", lastname: "Nicks"},
+	    {firstname: "Bob", lastname: "Barker"}
+	];
 
-	console.log($("#my-app"));
-	console.log(data);
+	var FamousPeopleList = can.Control.extend({
+		init: function ( element, opt) {
+			this.options.famousPeople = new can.List(opt.famousPeople);
+			this.element.html( can.view("famous-people-template", {
+				famousPeople: this.options.famousPeople
+			}));
+
+		},
+		'li click': function (li, event) {
+			var famousPeople = this.options.famousPeople;
+			var person = li.data("person");
+			var index = famousPeople.indexOf(person);
+			famousPeople.splice(index, 1);
+		}
+	});
+
+	new FamousPeopleList("#famouse-people-container", {famousPeople: famousPeople});
+
+	// Can Component
+	can.Component.extend({
+		tag: "people",
+	    template: '<ul>' +
+	                '{{#each people}}' +
+	                '<li can-click="remove">' +
+	                    '{{lastname}}, {{firstname}}' +
+	                '</li>' +
+	                '{{/each}}' +
+	                '</ul>',
+	    scope: {
+	    	people: people,
+	    	remove: function ( person ) {
+	    		var people = this.attr("people");
+	    		var index = people.indexOf(person);
+	    		people.splice(index, 1);
+	    	}
+	    }
+	});
+
+	var componentFrag = can.view("#people-component", {people: people});
+	$("#people-component-container").html(componentFrag);
 
 	// TODO:
-	// create list and display
+	// * create list and display
 	// pull list using ajax
-	// create controller
+	// * create controller
